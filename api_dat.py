bcbc4278e9c9bd9b947e3435f2873379a86d3d94
Adrianonsare <aonsare20@aiip.alueducation.com>
2020-09-25 18:39:48 +0300

    First Commit

---
+++ api_dat.py
@@ -0,0 +1,54 @@
+def read_api():
+    ''' takes api urls, reads into json'''
+    
+    #Solar
+    import requests
+    import pandas as pd
+    #Takes the url
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=-19.461907&lon=142.110216&exclude=[current,minutely,hourly]&appid=686a8268d2d60adfa1efd1b0f3d7ffe5"
+    url1="https://api.openweathermap.org/data/2.5/onecall?lat=53.556563&lon=8.598084&exclude=[current,minutely,hourly]&appid=686a8268d2d60adfa1efd1b0f3d7ffe5"
+
+    #Website Actions
+    jsonData = requests.get(url).json()
+    jsonData1 = requests.get(url1).json()
+
+
+    #import daily forecast data
+
+    d_date = pd.DataFrame(jsonData['daily']) 
+    d_date1 = pd.DataFrame(jsonData1['daily']) 
+
+  
+    #Rebuild initial dataframe
+    df_solar=pd.concat([d_date.drop(['temp'], axis=1), d_date['temp'].apply(pd.Series)], axis=1)
+    df_wind=pd.concat([d_date1.drop(['temp'], axis=1), d_date1['temp'].apply(pd.Series)], axis=1)
+
+
+    #Converting date column in data imported from API to Day and month columns resembling training dataset
+    df_solar['Day']=pd.to_datetime(df_solar.iloc[:,0],unit = 's').dt.day
+    df_solar['Month']=pd.to_datetime(df_solar.iloc[:,0],unit = 's').dt.month
+    df_solar=df_solar.iloc[0:7,]
+
+    df_wind['Day']=pd.to_datetime(df_wind.iloc[:,0],unit = 's').dt.day
+    df_wind['Month']=pd.to_datetime(df_wind.iloc[:,0],unit = 's').dt.month
+
+
+    df_solar=df_solar.rename(columns={"wind_speed":"wind speed","wind_deg":"direction","max": "Temp_Hi","min": "Temp_Low",
+                                    "clouds": "Cloud_Cover_Percentage","uvi":"Solar"})
+
+    df_solar.drop(columns=['sunrise','sunset','feels_like','pressure',
+                            'humidity','dew_point','weather','dt','pop',
+                            'night','eve','morn','wind speed','direction'],inplace=True)
+
+    df_wind=df_wind.rename(columns={"wind_speed":"wind speed","wind_deg":"direction","max": "Temp_Hi","min": "Temp_Low",
+                                    "clouds": "Cloud_Cover_Percentage","uvi":"Solar","rain": "Rainfall_in_mm"})
+
+    df_wind.drop(columns=['sunrise','sunset','feels_like','pressure',
+                            'humidity','dew_point','weather','dt','pop',
+                            'night','eve','morn','Cloud_Cover_Percentage','Solar','Temp_Low',
+                        'Temp_Hi','day'],inplace=True)
+
+    #Slice dataframes from weather API to have separate wind and solar dataframes
+    df_solar = df_solar[['Month','Day','Temp_Hi','Temp_Low','Solar','Cloud_Cover_Percentage']]
+    df_wind = df_wind[['Month','Day','wind speed','direction']]
+    return df_solar,df_wind 
---
+++ model.py
@@ -0,0 +1,102 @@
+#Import libraries
+import pandas as pd
+import  numpy as np
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import pickle
+import requests
+import datetime as dt
+
+
+
+solar=pd.read_csv('/home/adrian/Desktop/NOTEBOOKS/ALU_FLASK/MyProject/summ_data/solar_generation_data.csv')
+
+wind=pd.read_csv('/home/adrian/Desktop/NOTEBOOKS/ALU_FLASK/MyProject/summ_data/wind_generation_data.csv')
+
+#In the original dataset, wind data had 366 days, and did not have a month,Day column
+#Last row dropped to make the length of the dataframe=365
+#Month and day columns added to wind dataset
+wind=wind[:-1]
+Month = solar["Month"]
+Day= solar["Day"]
+wind = wind.join(Month)
+wind = wind.join(Day)
+
+#Rearrange ind dataset columns
+wind=wind[['Month','Day','wind speed','direction','Power Output']]
+
+
+#Mapping month string values to numeric
+month_vals={'Jan':1,'Feb':2,'Mar':3,'Apr':4,
+            'May':5,'Jun':6,'Jul':7,'Aug':8,
+            'Sep':9,'Oct':10,'Nov':11,'Dec':12}
+
+
+solar['Month']=solar.iloc[:,0].map(month_vals)
+wind['Month']=wind.iloc[:,0].map(month_vals)
+
+#Additional data preparation
+solar.rename(columns={'Temp Hi': 'Temp_Hi','Temp Low': 'Temp_Low',
+'Cloud Cover Percentage': 'Cloud_Cover_Percentage',
+'Power Generated in MW':'Nom.Power_Generated_in_MW'},inplace=True)
+
+
+#Convert data with degree symbol from string to float
+#drop rainfall column
+solar['Temp_Hi']=solar['Temp_Hi'].str.split("°", n = 1, expand = True)
+solar['Temp_Low']=solar['Temp_Low'].str.split("°", n = 1, expand = True)
+solar[['Temp_Hi','Temp_Low']] = solar[['Temp_Hi','Temp_Low']].astype(float)
+solar=solar.drop(columns=['Rainfall in mm'])
+
+#Fill out Missing Values
+solar=solar.fillna(0)
+wind=wind.fillna(0)
+
+#Split data into features and target
+X1 = solar.iloc[:, 0:6]
+y1 = solar.iloc[:, -1]
+
+X2 = wind.iloc[:, 0:4]
+y2 = wind.iloc[:, -1]
+
+
+#Split data into training and test sets
+#solar
+X_train, X_test, y_train, y_test = train_test_split(X1, y1, test_size=0.2, random_state=0)
+#Wind
+X1_train, X1_test, y1_train, y1_test = train_test_split(X2, y2, test_size=0.2, random_state=0)
+
+
+#Initiate model
+regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+regressor1 = RandomForestRegressor(n_estimators=20, random_state=0)
+#Solar
+regressor.fit(X_train, y_train)
+# y_pred = regressor.predict(X_test)
+
+#Wind
+regressor1.fit(X1_train, y1_train)
+# y_pred1 = regressor1.predict(X1_test)
+
+# Saving model to disk
+# joblib.dump(regressor, "regressor_solar.pkl")
+# joblib.dump(regressor1, "regressor_wind.pkl")
+
+pickle.dump(regressor, open('model_solar.pkl','wb'))
+pickle.dump(regressor1, open('model_wind.pkl','wb'))
+
+
+# pickle.dump(regressor, open('model.pkl','wb'))
+# pickle.dump(regressor1, open('model1.pkl','wb'))
+
+# # Loading model to compare the results
+# model = pickle.load(open('model.pkl','rb'))
+# model1 = pickle.load(open('model1.pkl','rb'))
+# print(model.predict([[1.8]]))
+
+
+
